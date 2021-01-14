b5c9dae125771e55a1a4f3e0c6c2a8911fde0e8b
Jimmy <jimmyfarleydev@gmail.com>
2021-01-14 02:06:39 -0500

    init

---
+++ src/utils/Config.ts
@@ -0,0 +1,9 @@
+export const Config = {
+  site_name: 'Jimmy Blog',
+  title: 'Jimmy Blog',
+  description: 'Personal Blog for Jimmy',
+  url: 'https://example.com',
+  locale: 'en',
+  author: 'Jimmy',
+  pagination_size: 5,
+};
